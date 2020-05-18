266dedd68cae03e528a50aa589fa8c610719d736
JoachimBrasier <jo-brasier@outlook.com>
2020-05-18 17:22:00 +0200

    Mise en place du projet

---
+++ src/popup.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Typography } from "antd";
+import "antd/lib/typography/style/css";
+
+const Popup = () => <Typography.Text>Biblidev</Typography.Text>;
+
+export default Popup;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  target: "web",
+  devtool: "source-map",
+  node: {
+    fs: "empty",
+    net: "empty",
+  },
+  entry: {
+    navigate: "./src/navigate.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_module/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: "css-loader",
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "./src", globOptions: { ignore: "*.js" } },
+        {
+          from:
+            "./node_modules/webextension-polyfill/dist/browser-polyfill.min.js",
+        },
+        {
+          from:
+            "./node_modules/webextension-polyfill/dist/browser-polyfill.min.js.map",
+        },
+      ],
+    }),
+    new ExtractTextPlugin("styles.css"),
+    new UglifyJsPlugin({
+      sourceMap: true,
+    }),
+  ],
+};
