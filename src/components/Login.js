6d314d5504ce906a75f692f30033a596ce9e886f
JoachimBrasier <jo-brasier@outlook.com>
2020-05-21 13:47:42 +0200

    Ajout de la connexion/déconnexion

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,109 @@
-import React from "react";
-import { Input, Button, Form } from "antd";
+import React, { useState } from "react";
+import Axios from "axios";
+import { Input, Button, Form, message, Alert } from "antd";
 import Logo from "../assets/logos/logo_inline.svg";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import "antd/lib/form/style/css";
 import "antd/lib/input/style/css";
 import "antd/lib/button/style/css";
+import "antd/lib/message/style/css";
+import "antd/lib/alert/style/css";
 import "./Login.css";
 
 const { Item } = Form;
 const { Password } = Input;
 
 const Login = () => {
+  const [values, setValues] = useState({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    if (event.persist) event.persist();
+
+    setValues((previous) => ({
+      ...previous,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const saveToken = (token) => {
+    setErrors({});
+    browser.storage.local.set({ token });
+    return setLoading(false);
+  };
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      const response = await Axios.post(
+        "http://localhost:3000/api/plugin/login",
+        values
+      );
+
+      return saveToken(response.data.data.token);
+    } catch (error) {
+      setLoading(false);
+
+      if (error.response.data.message) {
+        return setErrors({ message: error.response.data.message });
+      }
+
+      return setErrors(error.response.data.data);
+    }
+  };
+
   return (
-    <Form noValidate>
+    <Form noValidate onFinish={handleSubmit} style={{ width: 300 }}>
       <div className="logo">
         <Logo />
       </div>
-      <Item name="email" style={{ marginBottom: 10 }}>
+      {errors.message && (
+        <Alert
+          type="error"
+          showIcon
+          message={errors.message}
+          style={{ maxWidth: "100%", marginBottom: 10 }}
+        />
+      )}
+      <Item
+        style={{ marginBottom: 10 }}
+        hasFeedback={!!errors.email}
+        validateStatus={!!errors.email && "error"}
+        help={!!errors.email && errors.email}
+      >
         <Input
+          name="email"
           placeholder="Adresse mail"
           prefix={<UserOutlined />}
-          style={{ width: 250 }}
+          style={{ width: "100%" }}
+          value={values.email}
+          onChange={handleChange}
+          disabled={loading}
         />
       </Item>
-      <Item name="password" style={{ marginBottom: 10 }}>
+      <Item
+        style={{ marginBottom: 0 }}
+        hasFeedback={!!errors.password}
+        validateStatus={!!errors.password && "error"}
+        help={!!errors.password && errors.password}
+      >
         <Password
+          name="password"
           placeholder="Mot de passe"
           prefix={<LockOutlined />}
-          style={{ width: 250 }}
+          style={{ width: "100%" }}
+          value={values.password}
+          onChange={handleChange}
+          disabled={loading}
         />
       </Item>
-      <Button block type="primary">
+      <Form.Item style={{ margin: 0, marginBottom: 10, textAlign: "right" }}>
+        <a href="https://biblidev.fr/mot-de-passe-oublie" target="_blank">
+          Mot de passe oublié ?
+        </a>
+      </Form.Item>
+      <Button htmlType="submit" block type="primary" loading={loading}>
         Connexion
       </Button>
     </Form>
--- src/popup.js
+++ src/popup.js
@@ -1,18 +1,71 @@
 import React, { useEffect, useState } from "react";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import "antd/lib/spin/style/css";
 import Login from "./components/Login";
+import Axios from "axios";
 
 const isChrome = navigator.userAgent.indexOf("Chrome") != -1;
 
 const Popup = () => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const handleLogout = () => {
+    browser.storage.local.remove("token");
+    return setUser(null);
+  };
+
+  const loginFromToken = () => {
+    const authToken = browser.storage.local.get("token");
+
+    authToken.then(async ({ token }) => {
+      if (token) {
+        try {
+          const response = await Axios.get(
+            `http://localhost:3000/api/plugin/user/${token}`
+          );
+
+          setUser(response.data.data.user);
+        } catch (error) {
+          setUser(null);
+          browser.storage.local.remove("token");
+        }
+
+        return setLoading(false);
+      } else {
+        setUser(null);
+        return setLoading(false);
+      }
+    }, null);
+  };
 
   useEffect(() => {
-    const authToken = browser.storage.local.get("auth");
-
-    authToken.then(({ auth }) => {}, null);
+    loginFromToken();
   }, []);
 
-  return <div style={{ padding: 30 }}>{user ? "Connecté" : <Login />}</div>;
+  useEffect(() => {
+    browser.storage.onChanged.addListener(loginFromToken);
+
+    return () => {
+      browser.storage.onChanged.removeListener(loginFromToken);
+    };
+  }, []);
+
+  return (
+    <div style={{ padding: 30 }}>
+      {loading ? (
+        <Spin indicator={<LoadingOutlined />} />
+      ) : user ? (
+        <div>
+          <p>{`Bonjour ${user.username}`}</p>
+          <button onClick={handleLogout}>logout</button>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
 };
 
 export default Popup;
